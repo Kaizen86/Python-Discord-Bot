4308740f0ea271a27f34b9ea8550b61ef049561e
Daniel Bridgewater <danielbridgewater87@gmail.com>
2019-02-12 22:10:07 +0000

    Replaces messy punctuation detection with builtin functions.

--- modules/shadow_translator.py
+++ modules/shadow_translator.py
@@ -1,6 +1,6 @@
 from bidict import bidict
 
-class ShadowTranslator:			
+class ShadowTranslator:
 	def __init__(self):
 		#define translations for each word, then convert the dictionary to be bidirectional
 		rawdata = {
@@ -30,55 +30,51 @@ class ShadowTranslator:
 		"XUEI": "X",
 		"QUAL": "Y",
 		"ZIIE": "Z",
-		
+
 		" ": " ", #Space
 		}
 		self.__translations__ = bidict(rawdata)
-		self.__punctuation__ = ["¬","`","!","\"","£","$","%","^","&","*","(",")","_","-","=","+","[","]","{","}",";","'","#",":","@","~",",",".","/","\"","<",">","?","|","1","2","3","4","5","6","7","8","9","0"]
-		
+
 	def ConvertToShadow(self, phrase):
 		string = ""
 		for letter in phrase.upper():
 			if letter is "":
 				print("blank")
 				string += "  " #spaces should be symbolised as two spaces in shadow
-			
+
 			try:
 				string += self.__translations__.inv[letter] + " " #add the translated character with a space to separate shadow words
 			except:
-				if self.__punctuation__.__contains__(letter): #check for punctuation
-					string += letter #add the character
-				else:
-					string += "?" #unknown character
-						
+				string += letter #letter was not in lookup; it is probably punctuation
+
 		return string
 	def ConvertFromShadow(self, shadow):
 		string = ""
-		
+
 		#guide to variable names:
 		#words = english words in shadow as array
 		#word = specific english word in shadow
 		#letters = list of shadow words as array
 		#letter = individual shadow word
-		
+
 		words = shadow.split("   ")
 		for word in words:
 			letters = word.split()
 			for letter in letters:
 				foundpunctuation = []
-				#if any punctuation is found in a shadow letter, we need to remove it for the lookup then add it back later.found
+				#if any punctuation is found in a shadow letter, we need to remove it for the lookup then add it back later.
 				for character in letter:
-					if self.__punctuation__.__contains__(character):
+					if not character.isalnum():
 						#make a note
 						foundpunctuation.append(character)
 						#remove from string
 						letter = letter.replace(character,"")
 				try:
+					for character in foundpunctuation: #add punctuation back
+						string += character
 					string += self.__translations__[letter] #add the translated character
 				except:
-					if foundpunctuation.__contains__(letter) == False: #check for punctuation
+					if not letter.isalpha(): #check for punctuation
 						string += "?" #unknown character
-				for character in foundpunctuation: #add punctuation back
-					string += character
 			string += " " #spaces between words
 		return string
