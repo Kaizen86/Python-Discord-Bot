323a23c541290f9cec8307f9cab885dacdddb26d
Daniel Bridgewater <danielbridgewater87@gmail.com>
2019-08-15 22:10:43 +0100

    Oops. I seem to have deleted the code for the 'meep' triggerword.

--- bot.py
+++ bot.py
@@ -270,7 +270,9 @@ async def on_message(message):
 
 		#triggerwords
 		msg_lowercase = message.content.lower()
-		if isWholeWordInString(msg_lowercase, "meep")
+		if isWholeWordInString(msg_lowercase, "meep"):
+			await message.channel.send("Meep")
+			userData.set_user_data(message.author.id,"meeps",int(userData.get_user_data(message.author.id,"meeps"))+1)
 		if isWholeWordInString(msg_lowercase, "wheatley") and isWholeWordInString(msg_lowercase, "moron"): #message must have the words "wheatley" and "moron" to trigger.
 				await message.channel.send("I AM NOT A MORON!")
 		if isWholeWordInString(msg_lowercase, "pineapple"):
