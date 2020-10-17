679ad754087dd1222b5258d06f71107a9fa6bf19
Blattoid <danielbridgewater87@gmail.com>
2020-10-17 02:27:07 +0100

    Makes output for reloadallext pretty and adds loadext command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,21 +1,39 @@
-import discord
 from discord.ext import commands
 from discord import Status
+from traceback import format_exc
 from os import _exit as force_exit
 
 class AdminCog(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.shutdown.description = "Shuts the bot down."
 
 	@commands.command()
-	async def reloadext(self, ctx):
+	async def reloadallext(self, ctx):
 		msg = await ctx.send("Reloading all extensions...")
+		print("ALL EXTENSION RELOAD INITIATED")
 		#We have to make a copy to avoid changing the thing we are iterating over as we go
 		extensions = self.bot.extensions.copy()
+		output = ""
 		for extension in extensions:
-			self.bot.reload_extension(extension)
-			await msg.edit(content=msg.content+"\n\t"+extension)
-		await msg.edit(content=msg.content+"\nOperation finished.")
+			try: self.bot.reload_extension(extension)
+			except: success = False
+			else: success = True
+			output += "\n\t{0}:\t[{1}]".format(extension, "OK" if success else "ERR")
+		await msg.edit(content=msg.content+output+"\nOperation finished.")
+
+	@commands.command()
+	async def loadext(self, ctx, *, extension: str):
+		msg = await ctx.send("Loading extension "+extension)
+		print("Loading extension "+extension)
+		output = ""
+		try: self.bot.load_extension(extension)
+		except:
+			success = False
+			output += format_exc()
+		else: success = True
+		output += "\n\t{0}: [{1}]".format(extension, "OK" if success else "ERR")
+		await msg.edit(content=msg.content+output+"\nOperation finished.")
 
 	@commands.command()
 	async def shutdown(self, ctx):
