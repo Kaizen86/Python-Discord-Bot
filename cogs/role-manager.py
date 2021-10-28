338f520a5af0f070807a76a85167b1f166680de1
Blattoid <danielbridgewater87@gmail.com>
2021-10-28 14:30:34 +0100

    Consolidate urllib import statements

--- cogs/role-manager.py
+++ cogs/role-manager.py
@@ -5,8 +5,7 @@ from discord.ext import commands
 from discord.utils import get
 import database.Database as db
 import re
-import urllib.request
-import urllib.parse
+import urllib
 import json
 
 def time():
